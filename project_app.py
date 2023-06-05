a31b5bdb4079c80f356370957d2f05d9ed2e4594
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-05 19:27:43 +0300

    Update project_app.py

--- project_app.py
+++ project_app.py
@@ -19,7 +19,7 @@ def minus(value, key):
 def main():
     st.title("Bank Information Classification")
     
-    col1, col2, col3 = st.columns([11, 1, 1])
+    col1, col2, col3 = st.columns([10, 1.4, 1.4])
     with col1:
        
         input_data = {
@@ -41,8 +41,8 @@ def main():
             'emp.var.rate': st.slider("Enter employment variation rate:", min_value=-5.0, max_value=5.0, value=0.0, step=0.1, key='emp.var.rate'),
             'cons.price.idx': st.slider("Enter consumer price index:", min_value=0.0, max_value=105.0, value=0.0, step=0.1, key='cons.price.idx'),
             'cons.conf.idx': st.slider("Enter consumer confidence index:", min_value=-100.0, max_value=100.0, value=0.0, step=0.1, key='cons.conf.idx'),    
-            'euribor3m': st.slider("Enter euribor 3 month rate:", min_value=0.0, max_value=7.0, value=0.0, step=0.01, key='euribor3m'),
-            'nr.employed': st.slider("Enter number of employees:", min_value=0.0, max_value=10000.0, value=0.0, step=10.0, key='nr.employed'),
+            'euribor3m': st.slider("Enter euribor 3 month rate:", min_value=0.0, max_value=7.0, value=0.0, step=0.001, key='euribor3m'),
+            'nr.employed': st.slider("Enter number of employees:", min_value=0, max_value=10000, value=0, step=10, key='nr.employed'),
         }
     with col2:
         st.write("")
@@ -54,6 +54,7 @@ def main():
         st.button("-1", on_click=minus, args=(1, 'duration'),key = "dur_rem_1")
         st.button("+1", on_click=plus, args=(1, 'campaign'),key = "cam_add_1")
         st.button("-1", on_click=minus, args=(1, 'campaign'),key = "cam_rem_1")
+        st.write("")
         st.button("+1", on_click=plus, args=(1, 'pdays'),key = "pdays_add_1")
         st.button("-1", on_click=minus, args=(1, 'pdays'),key = "pdays_rem_1")
         st.button("+1", on_click=plus, args=(1, 'previous'),key = "previous_add_1")
@@ -62,12 +63,12 @@ def main():
            st.write("")
         st.button("+1", on_click=plus, args=(1, 'emp.var.rate'),key = "emp.var.rate_add_1")
         st.button("-1", on_click=minus, args=(1, 'emp.var.rate'),key = "emp.var.rate_rem_1")
-        st.button("+1", on_click=plus, args=(1, 'cons.price.idx'),key = "cons.price.idx_add_1")
-        st.button("-1", on_click=minus, args=(1, 'cons.price.idx'),key = "cons.price.idx_rem_1")
-        st.button("+1", on_click=plus, args=(1, 'cons.conf.idx'),key = "cons.conf.idx_add_1")
-        st.button("-1", on_click=minus, args=(1, 'cons.conf.idx'),key = "cons.conf.idx_rem_1")
-        st.button("+1", on_click=plus, args=(1, 'euribor3m'),key = "euribor3m_add_1")
-        st.button("-1", on_click=minus, args=(1, 'euribor3m'),key = "euribor3m_rem_1")
+        st.button("+0.01", on_click=plus, args=(0.01, 'cons.price.idx'),key = "cons.price.idx_add_1")
+        st.button("-0.01", on_click=minus, args=(0.01, 'cons.price.idx'),key = "cons.price.idx_rem_1")
+        st.button("+0.1", on_click=plus, args=(0.1, 'cons.conf.idx'),key = "cons.conf.idx_add_1")
+        st.button("-0.1", on_click=minus, args=(0.1, 'cons.conf.idx'),key = "cons.conf.idx_rem_1")
+        st.button("+0.01", on_click=plus, args=(0.01, 'euribor3m'),key = "euribor3m_add_1")
+        st.button("-0.01", on_click=minus, args=(0.01, 'euribor3m'),key = "euribor3m_rem_1")
         st.button("+1", on_click=plus, args=(1, 'nr.employed'),key = "nr.employed_add_1")
         st.button("-1", on_click=minus, args=(1, 'nr.employed'),key = "nr.employed_rem_1")
         
@@ -82,6 +83,7 @@ def main():
         st.button("-5", on_click=minus, args=(5, 'duration'),key = "dur_rem_5")
         st.button("+5", on_click=plus, args=(5, 'campaign'),key = "cam_add_5")
         st.button("-5", on_click=minus, args=(5, 'campaign'),key = "cam_rem_5")
+        st.write("")
         st.button("+5", on_click=plus, args=(5, 'pdays'),key = "pdays_add_5")
         st.button("-5", on_click=minus, args=(5, 'pdays'),key = "pdays_rem_5")
         st.button("+5", on_click=plus, args=(5, 'previous'),key = "previous_add_5")
@@ -90,25 +92,27 @@ def main():
            st.write("")
         st.button("+5", on_click=plus, args=(5, 'emp.var.rate'),key = "emp.var.rate_add_5")
         st.button("-5", on_click=minus, args=(5, 'emp.var.rate'),key = "emp.var.rate_rem_5")
-        st.button("+5", on_click=plus, args=(5, 'cons.price.idx'),key = "cons.price.idx_add_5")
-        st.button("-5", on_click=minus, args=(5, 'cons.price.idx'),key = "cons.price.idx_rem_5")
-        st.button("+5", on_click=plus, args=(5, 'cons.conf.idx'),key = "cons.conf.idx_add_5")
-        st.button("-5", on_click=minus, args=(5, 'cons.conf.idx'),key = "cons.conf.idx_rem_5")
-        st.button("+5", on_click=plus, args=(5, 'euribor3m'),key = "euribor3m_add_5")
-        st.button("-5", on_click=minus, args=(5, 'euribor3m'),key = "euribor3m_rem_5")
+        st.button("+0.1", on_click=plus, args=(0.1, 'cons.price.idx'),key = "cons.price.idx_add_5")
+        st.button("-0.1", on_click=minus, args=(0.1, 'cons.price.idx'),key = "cons.price.idx_rem_5")
+        st.button("+1", on_click=plus, args=(1, 'cons.conf.idx'),key = "cons.conf.idx_add_5")
+        st.button("-1", on_click=minus, args=(1, 'cons.conf.idx'),key = "cons.conf.idx_rem_5")
+        st.button("+0.05", on_click=plus, args=(0.05, 'euribor3m'),key = "euribor3m_add_5")
+        st.button("-0.05", on_click=minus, args=(0.05, 'euribor3m'),key = "euribor3m_rem_5")
         st.button("+5", on_click=plus, args=(5, 'nr.employed'),key = "nr.employed_add_5")
         st.button("-5", on_click=minus, args=(5, 'nr.employed'),key = "nr.employed_rem_5")
 
     # Add a button to perform the classification
     if st.button("Perform Classification"):
         prediction = perform_classification(input_data)
-        st.subheader("Prediction:")
-        st.write(prediction)
+#         st.subheader("Prediction:")
+#         st.write(prediction)
+
+
 
 
 def perform_classification(input_data):
     # Load the trained model
-    model = joblib.load("bank_model.pkl")
+    model = joblib.load("bank_model1.pkl")
 
     # Prepare the input data for prediction
     X = prepare_input_data(input_data)
@@ -116,7 +120,15 @@ def perform_classification(input_data):
     # Perform the prediction
     prediction = model.predict(X)
 
-    return prediction
+    # Map the predicted values to text labels and colors
+    label_map = {0: ("You are not elligible for creating a deposit account", "rgba(255, 0, 0, 0.5)"), 1: ("You are elligible for creating a deposit account", "rgba(0, 255, 0, 0.5)")}
+    prediction_label, prediction_color = label_map[prediction[0]]
+
+    # Display the text box with colored background
+    st.markdown(f'<div style="background-color: {prediction_color}; padding: 20px;">{prediction_label}</div>',
+                unsafe_allow_html=True)
+
+#     return prediction_label
 
 
 def prepare_input_data(input_data):
